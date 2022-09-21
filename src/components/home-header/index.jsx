13939c5285544c25e2ab7d3b78bc4175dbaa8410
harryyan <harryyan@vectorbuilder.net>
2022-09-21 16:48:03 +0800

    commit

--- src/components/home-header/index.jsx
+++ src/components/home-header/index.jsx
@@ -1,15 +1,27 @@
-import { Affix } from 'antd';
-import React from 'react'
-import './index.css'
+import { Affix } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 function HomeHeader() {
   return (
     <Affix>
-    <div className='homeHeader'>
-      <div className='homeHeaderNav'>
-        <h2>HomeHeader Demo</h2>
+      <div className="homeHeader">
+        <div className="homeHeaderInner">
+          <div className="logo"></div>
+          <div className="homeHeaderNav">
+            <div className="homeHeaderNavItem">
+              <p className="active">首页</p>
+            </div>
+            <div className="homeHeaderNavItem">
+              <p className="nav">笔记</p>
+            </div>
+            <div className="homeHeaderNavItem">
+              <p className="nav">个人中心</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </Affix>
-  )
+  );
 }
 export default HomeHeader;
